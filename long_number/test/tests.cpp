04a448b24a16780be266b622b715ac5acdccfc61
EgorovaViktoria <vikaegor09@gmail.com>
2025-04-04 23:08:23 +0300

    long numbers

---
+++ long_number/test/tests.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include "long_number.hpp"
+
+int main() {
+    shella::LongNumber num1("37283723872283928382938");
+    shella::LongNumber num2("1293283928398292839283");
+    shella::LongNumber sum = num1 + num2;
+
+    std::cout << "Sum: " << sum << std::endl;
+
+    return 0;
+}
